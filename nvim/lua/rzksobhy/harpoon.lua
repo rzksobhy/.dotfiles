cb581bd6db4fa3cb4b943df58a4725febcf893ac
Sobhy Rzk <rzksobhy2@gmail.com>
2024-01-10 10:03:43 +0200

    change fzf color

--- nvim/lua/rzksobhy/harpoon.lua
+++ nvim/lua/rzksobhy/harpoon.lua
@@ -1,11 +1,11 @@
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file, opts)
+vim.keymap.set("n", "<leader>a", require("harpoon.mark").add_file, opts)
 
-vim.keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu, opts)
+vim.keymap.set("n", "<leader>h", require("harpoon.ui").toggle_quick_menu, opts)
 
-vim.keymap.set("n", "<leader>hj", require("harpoon.ui").nav_next, opts)
-vim.keymap.set("n", "<leader>hk", require("harpoon.ui").nav_prev, opts)
+vim.keymap.set("n", "<leader>j", require("harpoon.ui").nav_next, opts)
+vim.keymap.set("n", "<leader>k", require("harpoon.ui").nav_prev, opts)
 
 for i = 1, 9 do
     vim.keymap.set("n", "<leader>" .. i, string.format([[:lua require("harpoon.ui").nav_file(%i)<CR>]], i), opts)
